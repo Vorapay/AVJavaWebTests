00469ea4db73df5316a71838dfad1c6dcaf928fe
Vara <artemiivoropaev@gmail.com>
2026-03-24 09:43:11 +0700

    Change BasePage.

--- src/main/java/core/base/BasePage.java
+++ src/main/java/core/base/BasePage.java
@@ -1,7 +1,7 @@
 package core.base;
 
 import com.codeborne.selenide.SelenideElement;
-import core.pages.SearchGroups;
+import core.pages.SearchGroupsPage;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
@@ -12,11 +12,11 @@ public abstract class BasePage {
     protected SelenideElement searchField = $("[name='st.query']");
     protected SelenideElement vkServices = $("[data-l='t,vk_ecosystem']");
 
-    public SearchGroups search(String query) {
+    public SearchGroupsPage search(String query) {
         searchField.shouldBe(visible).setValue(query);
         SelenideElement searchGroup = $x(String.format("//a[contains(@class, 'toolbar_search_suggest-item') and contains(text(), '%s')]", query));
         searchGroup.scrollTo().click();
-        return new SearchGroups();
+        return new SearchGroupsPage();
     }
 
     public void openVkServices() {
